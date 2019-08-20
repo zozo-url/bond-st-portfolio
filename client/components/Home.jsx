703f676d541bac8b24eda574a8bc90678ac912af
zozo-url <zozo-url@protonmail.ch>
2019-08-20 17:01:17 +1200

    change to functional component

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -6,26 +6,18 @@ import house from '../../public/images/house.gif'
 // add video logo
 // css = centre everything
 // make responsive
-class Home extends React.Component {
-  constructor (props) {
-    super()
-    this.state = {
-    }
-  }
-
-  render () {
-    return (
-      <div className='home-container'>
-        <div className='space'> </div>
-        <img src={logo} className='logo' />
-        <img src={house} className='house' />
-        <div className='home-menu'>
-          <Link to='/info'><p className='subtitle'>INFO</p></Link>
-          <Link to='/edit'><p className='subtitle'>EDIT</p></Link>
-          <Link to='/unit'><p className='subtitle'>UNIT</p></Link>
-        </div>
+const Home = () => {
+  return (
+    <div className='home-container'>
+      <div className='space'> </div>
+      <img src={logo} className='logo' />
+      <img src={house} className='house' />
+      <div className='home-menu'>
+        <Link to='/info'><p className='subtitle'>INFO</p></Link>
+        <Link to='/edit'><p className='subtitle'>EDIT</p></Link>
+        <Link to='/unit'><p className='subtitle'>UNIT</p></Link>
       </div>
-    )
-  }
+    </div>
+  )
 }
 export default Home
