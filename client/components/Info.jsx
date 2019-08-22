35b2159630fa3d9a3fd9c5dcdebce69049f0810d
zozo-url <zozo-url@protonmail.ch>
2019-08-22 17:29:42 +1200

    add social links

--- client/components/Info.jsx
+++ client/components/Info.jsx
@@ -6,9 +6,9 @@ const Info = () => (
     <Nav />
     <p id='description'>This is a description about Bond St. Bond St hosts dance music events and makes video content during these events. They aim to make really cool visual content related to dance music and fun times.</p>
     <div className='socials'>
-      <i class='fab fa-facebook-square' />
-      <i class='fab fa-vimeo-square' />
-      <i class='fab fa-bandcamp' />
+      <a href='https://soundcloud.com/bond_st'><i class='fab fa-soundcloud' /></a>
+      <a href='https://vimeo.com/bondst'><i class='fab fa-vimeo-square' /></a>
+      <a href='https://www.instagram.com/bond__st/?hl=en'><i class='fab fa-instagram' /></a>
     </div>
   </div>
 )
