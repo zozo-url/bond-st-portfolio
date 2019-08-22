75103406278f4f8545ee20c29b619b6b140d071c
zozo-url <zozo-url@protonmail.ch>
2019-08-22 18:39:42 +1200

    align info page text

--- client/components/Info.jsx
+++ client/components/Info.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Nav from './Nav'
 
 const Info = () => (
-  <div className='info'>
+  <div>
     <Nav />
-    <p id='description'>This is a description about Bond St. Bond St hosts dance music events and makes video content during these events. They aim to make really cool visual content related to dance music and fun times.</p>
-    <div className='socials'>
-      <a href='https://soundcloud.com/bond_st'><i class='fab fa-soundcloud' /></a>
-      <a href='https://vimeo.com/bondst'><i class='fab fa-vimeo-square' /></a>
-      <a href='https://www.instagram.com/bond__st/?hl=en'><i class='fab fa-instagram' /></a>
+    <div className='space-left'>
+      <p>This is a description about Bond St. Bond St hosts dance music events and makes video content during these events. They aim to make really cool visual content related to dance music and fun times.</p>
+      <div className='socials'>
+        <a href='https://soundcloud.com/bond_st'><i className='fab fa-soundcloud' /></a>
+        <a href='https://vimeo.com/bondst'><i className='fab fa-vimeo-square' /></a>
+        <a href='https://www.instagram.com/bond__st/?hl=en'><i className='fab fa-instagram' /></a>
+      </div>
     </div>
   </div>
 )
